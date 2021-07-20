906f79f8d52a13226595dceb88da5b173ad49e96
李光浩 <liguanghao@viatomtech.com>
2021-07-20 12:40:36 +0800

    tyrt

--- app/src/main/java/com/vaca/rsa/MainActivity.kt
+++ app/src/main/java/com/vaca/rsa/MainActivity.kt
@@ -2,12 +2,14 @@ package com.vaca.rsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sleep(5000)
         RSAEncrypt.main()
     }
 }
